fffd264b3fadee2bd3e7524614c37e98246ca7b3
stella640 <stellabaddie75@gmail.com>
2026-01-20 15:52:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,9 @@ const allowedOrigins = [
   'http://yue.ct.ws',
   'http://liang.ct.ws',
   'https://access-psi.vercel.app',
-  'https://bell-dusky.vercel.app'
+  'https://bell-dusky.vercel.app',
+  'https://softi.ct.ws',
+  'https://lebda.ct.ws'
    // add all your trusted frontends here
 ];
 
